82851b71735722bdc8dfbe8fcd6c06be1f451485
Jay D <jdavenz8@gmail.com>
2023-10-04 20:26:38 +1300

    Added functionalities::Functionalities List:
    Text Input
    Bold Text
    Italic Text
    Underline Text
    Text Alignment
    Font Selection
    Emoji Selection
    Text Shadow Radius
    Text Shadow X-Offset
    Text Shadow Y-Offset
    Text Shadow Color
    Text Rotation Angle
    Link URL
    Highlight Color
    Text Color
    Background Color
    Alert Visibility

--- TextEdit/ContentView.swift
+++ TextEdit/ContentView.swift
@@ -6,6 +6,9 @@
 //
 
 
+
+
+///
 import SwiftUI
 
 struct ContentView: View {
@@ -13,52 +16,178 @@ struct ContentView: View {
     @State private var isBold = false
     @State private var isItalic = false
     @State private var isUnderlined = false
-
+    @State private var alignment: TextAlignment = .leading
+    @State private var selectedFont: String = "Helvetica Neue"
+    @State private var selectedEmoji: String = "😊"
+    @State private var shadowRadius: CGFloat = 0
+    @State private var shadowX: CGFloat = 0
+    @State private var shadowY: CGFloat = 0
+    @State private var shadowColor: Color = .black
+    @State private var rotationAngle: Double = 0
+    @State private var linkURL = ""
+    @State private var highlightColor: Color = .yellow
+    @State private var textColor: Color = .black
+    @State private var bgColor: Color = .white
+    @State private var showAlert = false
+    
+    var modifiedText: String {
+        // Modify the text based on user preferences (bold, italic, underline, etc.)
+        // You can format the text here using the selected options
+        return "Modified Text: \(text)"
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
                 ScrollView {
                     TextField("Enter text here", text: $text)
-                        .font(.body)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
-                        .background(Color.white)
-                        .cornerRadius(10)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.black, lineWidth: 1)
-                        )
-                        .multilineTextAlignment(.center)
-                        .padding()
-                        .textFieldStyle(RoundedBorderTextFieldStyle()) // Apply a rounded border style
-
-                    Text(text)
-                        .font(.body)
+                        .background(RoundedRectangle(cornerRadius: 10).stroke(Color.black))
+                        .multilineTextAlignment(alignment)
+                        .padding(1.0)
+                    
+                    Text(modifiedText)
                         .bold(isBold)
                         .italic(isItalic)
                         .underline(isUnderlined, color: .black)
+                        .font(Font.custom(selectedFont, size: 18))
+                        .foregroundColor(textColor)
+                        .padding()
+                        .background(bgColor)
+                        .cornerRadius(10)
+                        .shadow(color: shadowColor, radius: shadowRadius, x: shadowX, y: shadowY)
+                        .rotationEffect(Angle(degrees: rotationAngle))
+                        .background(highlightColor)
+                        .padding()
+                    
+                    Button(action: {
+                                 // Copy modified text to clipboard
+                                 UIPasteboard.general.string = modifiedText
+                                 showAlert = true // Show alert when text is copied
+                             }) {
+                                 Text("Copy Text")
+                                     .padding()
+                                     .background(Color.blue)
+                                     .foregroundColor(.white)
+                                     .cornerRadius(10)
+                             }
+                         
+                         .padding()
+                         .alert(isPresented: $showAlert) {
+                             Alert(
+                                 title: Text("Text Copied"),
+                                 message: Text("The modified text has been copied to the clipboard."),
+                                 dismissButton: .default(Text("OK"))
+                             )
+                         }
+                    
+                    ControlsView(isBold: $isBold, isItalic: $isItalic, isUnderlined: $isUnderlined,
+                                 alignment: $alignment, selectedFont: $selectedFont, selectedEmoji: $selectedEmoji,
+                                 shadowRadius: $shadowRadius, shadowX: $shadowX, shadowY: $shadowY,
+                                 shadowColor: $shadowColor, rotationAngle: $rotationAngle, linkURL: $linkURL,
+                                 highlightColor: $highlightColor, textColor: $textColor, bgColor: $bgColor,
+                                 showAlert: $showAlert)
                         .padding()
                 }
-
-                FormattingControlsView(isBold: $isBold, isItalic: $isItalic, isUnderlined: $isUnderlined)
-                    .padding()
+                .navigationBarTitle("Rich Text Editor")
+                .alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("Text Copied"),
+                        message: Text("The modified text has been copied to the clipboard."),
+                        dismissButton: .default(Text("OK"))
+                    )
+                }
             }
-            .navigationBarTitle("Rich Text Editor")
-            .padding()
         }
     }
 }
 
-struct FormattingControlsView: View {
+struct ControlsView: View {
     @Binding var isBold: Bool
     @Binding var isItalic: Bool
     @Binding var isUnderlined: Bool
-
+    @Binding var alignment: TextAlignment
+    @Binding var selectedFont: String
+    @Binding var selectedEmoji: String
+    @Binding var shadowRadius: CGFloat
+    @Binding var shadowX: CGFloat
+    @Binding var shadowY: CGFloat
+    @Binding var shadowColor: Color
+    @Binding var rotationAngle: Double
+    @Binding var linkURL: String
+    @Binding var highlightColor: Color
+    @Binding var textColor: Color
+    @Binding var bgColor: Color
+    @Binding var showAlert: Bool
+    
     var body: some View {
-        HStack {
+        VStack(spacing: 20) {
+            
             Toggle("Bold", isOn: $isBold)
             Toggle("Italic", isOn: $isItalic)
             Toggle("Underline", isOn: $isUnderlined)
+            
+            Picker("Text Alignment", selection: $alignment) {
+                Text("Left").tag(TextAlignment.leading)
+                Text("Center").tag(TextAlignment.center)
+                Text("Right").tag(TextAlignment.trailing)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
+            Picker("Font Family", selection: $selectedFont) {
+                Text("Helvetica Neue").tag("Helvetica Neue")
+                Text("Times New Roman").tag("Times New Roman")
+                Text("Courier New").tag("Courier New")
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
+            Picker("Emoji", selection: $selectedEmoji) {
+                Text("😊").tag("😊")
+                Text("🚀").tag("🚀")
+                Text("🎉").tag("🎉")
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
+            Text("Shadow Radius: \(Int(shadowRadius))")
+            Slider(value: $shadowRadius, in: 0...10, step: 1)
+            
+            Text("Shadow X: \(Int(shadowX))")
+            Slider(value: $shadowX, in: -10...10, step: 1)
+            
+            Text("Shadow Y: \(Int(shadowY))")
+            Slider(value: $shadowY, in: -10...10, step: 1)
+            
+            ColorPicker("Shadow Color", selection: $shadowColor)
+                           .padding()
+                       
+                       Slider(value: $rotationAngle, in: 0...360, step: 1)
+                       Text("Rotation Angle: \(Int(rotationAngle))°")
+                           .padding()
+                       
+                       TextField("Enter link URL", text: $linkURL)
+                           .textFieldStyle(RoundedBorderTextFieldStyle())
+                           .padding()
+                       
+                       ColorPicker("Text Highlight Color", selection: $highlightColor)
+                           .padding()
+                       
+                       ColorPicker("Text Color", selection: $textColor)
+                           .padding()
+                       
+                       ColorPicker("Background Color", selection: $bgColor)
+                           .padding()
+     
         }
         .padding()
     }
+
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+///////
+
