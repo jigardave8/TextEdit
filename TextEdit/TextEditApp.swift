0145c93dc63076020cd5369aa514a34890846834
Jay D <jdavenz8@gmail.com>
2023-10-04 16:55:01 +1300

    Initial Commit

---
+++ TextEdit/TextEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextEditApp.swift
+//  TextEdit
+//
+//  Created by Jigar on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TextEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
