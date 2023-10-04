5996b19bd261ac1846d1398ee1ae8b885aba67a1
Jay D <jdavenz8@gmail.com>
2023-10-04 18:47:52 +1300

    Basic Text edit functionalities added

--- TextEdit/ContentView.swift
+++ TextEdit/ContentView.swift
@@ -5,20 +5,60 @@
 //  Created by Jigar on 04/10/23.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text = ""
+    @State private var isBold = false
+    @State private var isItalic = false
+    @State private var isUnderlined = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                ScrollView {
+                    TextField("Enter text here", text: $text)
+                        .font(.body)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .textFieldStyle(RoundedBorderTextFieldStyle()) // Apply a rounded border style
+
+                    Text(text)
+                        .font(.body)
+                        .bold(isBold)
+                        .italic(isItalic)
+                        .underline(isUnderlined, color: .black)
+                        .padding()
+                }
+
+                FormattingControlsView(isBold: $isBold, isItalic: $isItalic, isUnderlined: $isUnderlined)
+                    .padding()
+            }
+            .navigationBarTitle("Rich Text Editor")
+            .padding()
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct FormattingControlsView: View {
+    @Binding var isBold: Bool
+    @Binding var isItalic: Bool
+    @Binding var isUnderlined: Bool
+
+    var body: some View {
+        HStack {
+            Toggle("Bold", isOn: $isBold)
+            Toggle("Italic", isOn: $isItalic)
+            Toggle("Underline", isOn: $isUnderlined)
+        }
+        .padding()
+    }
 }
